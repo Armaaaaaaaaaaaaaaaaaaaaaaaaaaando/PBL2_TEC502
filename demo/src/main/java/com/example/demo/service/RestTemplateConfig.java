c9f0dd439da98be368062647f3eaefb2c0c82fa1
Armando <111616936+Armaaaaaaaaaaaaaaaaaaaaaaaaaaando@users.noreply.github.com>
2024-11-03 16:53:44 -0300

    :docs: comentando o código

--- demo/src/main/java/com/example/demo/service/RestTemplateConfig.java
+++ demo/src/main/java/com/example/demo/service/RestTemplateConfig.java
@@ -4,11 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+// Indica que esta classe fornece configurações para o contexto da aplicação Spring
 @Configuration
 public class RestTemplateConfig {
 
+    // Define um bean de RestTemplate, que pode ser injetado em outras classes da aplicação
     @Bean
     public RestTemplate restTemplate() {
+        // Cria e retorna uma nova instância de RestTemplate
+        // O RestTemplate é usado para fazer chamadas HTTP e facilita a comunicação com APIs externas
         return new RestTemplate();
     }
 }
