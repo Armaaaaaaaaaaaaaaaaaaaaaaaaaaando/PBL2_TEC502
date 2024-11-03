129d8d940a70dcc44d00b79249f9d90b1ad7984b
Armando <111616936+Armaaaaaaaaaaaaaaaaaaaaaaaaaaando@users.noreply.github.com>
2024-11-03 16:52:38 -0300

    docs: comentando código

--- demo/src/main/java/com/example/demo/service/AppConfig.java
+++ demo/src/main/java/com/example/demo/service/AppConfig.java
@@ -5,13 +5,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
+// Indica que esta classe contém configurações para o contexto da aplicação Spring
 @Configuration
 public class AppConfig {
 
-     @Bean(name = "restTemplateApp")
+    // Define um bean chamado "restTemplateApp" que estará disponível para injeção na aplicação
+    @Bean(name = "restTemplateApp")
     public RestTemplate restTemplateApp() {
+        // Cria uma instância de RestTemplate, que é usada para fazer chamadas HTTP
         RestTemplate restTemplate = new RestTemplate();
+
+        // Adiciona um conversor de mensagens para JSON usando Jackson
+        // Isso permite que o RestTemplate converta automaticamente JSON para objetos Java e vice-versa
         restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+
+        // Retorna a instância configurada de RestTemplate
         return restTemplate;
     }
 }
